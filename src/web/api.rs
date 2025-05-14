00c7f4acb2f2e164155af8e55c422602afffaa66
Canmi <9997200@qq.com>
2025-05-14 10:05:10 +0800

    add(api): remove delete method

--- src/web/api.rs
+++ src/web/api.rs
@@ -1,10 +1,8 @@
 use axum::{Json, extract::Json as ExtractJson};
 use chrono::Local;
 use serde::{Serialize, Deserialize};
-//use std::time::Instant;
-
 use crate::web::response::ApiResponse;
-use crate::config::volume::{load_config, try_add_existing_volume, try_create_new_volume};
+use crate::config::volume;
 
 #[derive(Serialize)]
 struct Volume {
@@ -22,14 +20,16 @@ pub struct VolumeInput {
     path: String,
 }
 
+#[derive(Deserialize)]
+pub struct VolumeValue {
+    value: String,
+}
 
+// GET /v1/ipelfs/volumes
 pub async fn get_volumes() -> Json<ApiResponse<VolumeList>> {
-    
-    //let start = Instant::now();
-
-    let volumes = match load_config() {
+    let volumes = match volume::load_config() {
         Ok(cfg) => cfg.get_volume_map()
-            .into_iter()
+            .iter()
             .map(|(id, path)| Volume {
                 id: id.to_string(),
                 path: path.to_string(),
@@ -46,16 +46,40 @@ pub async fn get_volumes() -> Json<ApiResponse<VolumeList>> {
     Json(ApiResponse::ok(VolumeList { volumes }, Some(meta)))
 }
 
-pub async fn post_add_volume(Json(input): ExtractJson<VolumeInput>) -> Json<ApiResponse<String>> {
-    match try_add_existing_volume(&input.path) {
+// POST /v1/ipelfs/volumes/create
+pub async fn post_create_volume(Json(input): ExtractJson<VolumeInput>) -> Json<ApiResponse<String>> {
+    match volume::try_create_new_volume(&input.path) {
         Ok(id) => Json(ApiResponse::ok(id, None)),
         Err(msg) => Json(ApiResponse::fail(msg)),
     }
 }
 
-pub async fn post_create_volume(Json(input): ExtractJson<VolumeInput>) -> Json<ApiResponse<String>> {
-    match try_create_new_volume(&input.path) {
+// POST /v1/ipelfs/volumes/add
+pub async fn post_add_volume(Json(input): ExtractJson<VolumeInput>) -> Json<ApiResponse<String>> {
+    match volume::try_add_existing_volume(&input.path) {
         Ok(id) => Json(ApiResponse::ok(id, None)),
         Err(msg) => Json(ApiResponse::fail(msg)),
+    }
+}
+
+// POST /v1/ipelfs/volumes/remove
+pub async fn post_remove_volume(Json(input): ExtractJson<VolumeValue>) -> Json<ApiResponse<String>> {
+    let res = if input.value.starts_with('/') {
+        volume::remove_volume_by_path(&input.value)
+    } else {
+        volume::remove_volume_by_id(&input.value)
+    };
+
+    match res {
+        Ok(_) => Json(ApiResponse::ok(input.value, None)),
+        Err(err) => Json(ApiResponse::fail(err.to_string())),
+    }
+}
+
+// POST /v1/ipelfs/volumes/delete
+pub async fn post_delete_volume(Json(input): ExtractJson<VolumeValue>) -> Json<ApiResponse<String>> {
+    match volume::delete_ipelfs(&input.value) {
+        Ok(_) => Json(ApiResponse::ok(input.value, None)),
+        Err(err) => Json(ApiResponse::fail(err.to_string())),
     }
 }
--- src/web/routes.rs
+++ src/web/routes.rs
@@ -1,13 +1,20 @@
 use axum::{Router, routing::{get, post}};
-use crate::web::api::{get_volumes, post_add_volume, post_create_volume};
+
+use crate::web::api::{
+    get_volumes,
+    post_create_volume,
+    post_add_volume,
+    post_remove_volume,
+    post_delete_volume,
+};
 
 pub fn build_router() -> Router {
     Router::new()
-        .nest(
-            "/v1/ipelfs",
-            Router::new()
-                .route("/volumes", get(get_volumes))
-                .route("/volumes/add", post(post_add_volume))
-                .route("/volumes/create", post(post_create_volume))
+        .nest("/v1/ipelfs", Router::new()
+            .route("/volumes", get(get_volumes))
+            .route("/volumes/create", post(post_create_volume))
+            .route("/volumes/add", post(post_add_volume))
+            .route("/volumes/remove", post(post_remove_volume))
+            .route("/volumes/delete", post(post_delete_volume))
         )
 }
