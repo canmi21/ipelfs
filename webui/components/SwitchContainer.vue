0887e43ba3d2f0a0267a6b357101e2d336155277
Canmi <9997200@qq.com>
2025-05-16 12:56:04 +0800

    fix: switch box css

--- webui/components/SwitchContainer.vue
+++ webui/components/SwitchContainer.vue
@@ -7,7 +7,6 @@ import {
 import ActionSwitch from './ActionSwitch.vue'
 
 const { switches } = useGlobalSwitches()
-
 const getCurrentStateConfig = (swConfig: Readonly<SwitchConfig>): Readonly<SwitchStateConfig> => {
   const foundState = swConfig.states.find((s) => s.value === swConfig.currentStateValue)
   return foundState || swConfig.states[0]
@@ -17,15 +16,22 @@ const getCurrentStateConfig = (swConfig: Readonly<SwitchConfig>): Readonly<Switc
 <template>
   <div
     v-if="switches.length > 0"
-    class="absolute top-4 right-4 z-40 flex items-center flex-row-reverse space-x-1 space-x-reverse"
+    class="absolute top-4 right-4 z-40"
+    aria-label="Global page actions"
   >
-    <ActionSwitch
-      v-for="sw in switches"
-      :key="sw.id"
-      :icon-component="getCurrentStateConfig(sw).iconComponent"
-      :icon-class="getCurrentStateConfig(sw).iconClass"
-      :title="getCurrentStateConfig(sw).title"
-      :on-toggle="sw.onToggle"
-    />
+    <TransitionGroup
+      name="switch-list"
+      tag="div"
+      class="flex items-center flex-row-reverse gap-x-1"
+    >
+      <ActionSwitch
+        v-for="sw in switches"
+        :key="sw.id"
+        :icon-component="getCurrentStateConfig(sw).iconComponent"
+        :icon-class="getCurrentStateConfig(sw).iconClass"
+        :title="getCurrentStateConfig(sw).title"
+        :on-toggle="sw.onToggle"
+      />
+    </TransitionGroup>
   </div>
 </template>
