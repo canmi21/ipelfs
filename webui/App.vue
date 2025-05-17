813e6141f16e914a5326a5fa132c677dc3fe6a28
Canmi <9997200@qq.com>
2025-05-17 15:36:50 +0800

    add: vue localserver

---
+++ webui/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto p-4">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped></style>
---
+++ webui/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold text-blue-500">Welcome to Vue + Tailwind!</h1>
+    <p class="mt-4 text-lg text-gray-700">Your app is running smoothly.</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HomeView',
+}
+</script>
+
+<style scoped></style>
