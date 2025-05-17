31ac2d68055e792f2c52dda6cba3a9ac444f86f3
Canmi <9997200@qq.com>
2025-05-17 17:27:45 +0800

    add: dynamic title

--- webui/router/index.ts
+++ webui/router/index.ts
@@ -1,15 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { nextTick } from 'vue'
+
+import HomeView from '../views/HomeView.vue' // Insights
+import VolumesView from '../views/VolumesView.vue'
+import CollectionsView from '../views/CollectionsView.vue'
+import ActivityView from '../views/ActivityView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Insights',
       component: HomeView,
+      meta: { title: `Insights` },
+    },
+    {
+      path: '/volumes',
+      name: 'Volumes',
+      component: VolumesView,
+      meta: { title: `Volumes` },
+    },
+    {
+      path: '/collections',
+      name: 'Collections',
+      component: CollectionsView,
+      meta: { title: `Collections` },
+    },
+    {
+      path: '/activity',
+      name: 'Activity',
+      component: ActivityView,
+      meta: { title: `Activity` },
     },
   ],
+})
+
+router.afterEach((to) => {
+  nextTick(() => {
+    if (to.meta && to.meta.title) {
+      document.title = to.meta.title as string
+    } else {
+      document.title = 'ipelfs'
+    }
+  })
 })
 
 export default router
--- webui/views/VolumesView.vue
+++ webui/views/VolumesView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center">
     <h1 class="text-4xl font-bold text-blue-500">Welcome to Vue + Tailwind!</h1>
-    <p class="mt-4 text-lg text-gray-700">Your app is running smoothly. Insights</p>
+    <p class="mt-4 text-lg text-gray-700">Your app is running smoothly. VolumesView</p>
   </div>
 </template>
 
