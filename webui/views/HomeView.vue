e1e800349a57f6f0b36915994a7dd68be777effe
canmi21 <9997200@qq.com>
2025-05-17 16:28:47 +0800

    chore: resrc

--- webui/views/HomeView.vue
+++ webui/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <div class="text-center">
+    <h1 class="text-4xl font-bold text-blue-500">Welcome to Vue + Tailwind!</h1>
+    <p class="mt-4 text-lg text-gray-700">Your app is running smoothly.</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HomeView',
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+/* 这里可以加入额外的样式 */
+</style>
